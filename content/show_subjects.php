d358174f29acbb38b1a076353a46f948348c4e56
Woojin Jeon <69278138+jeonw70450@users.noreply.github.com>
2021-03-30 20:05:38 +1300

    Missing subject.php file????? Ask teacher

--- content/show_subjects.php
+++ content/show_subjects.php
@@ -1,38 +1,45 @@
-    <p>
-        <?php
-            $sub1_ID = $find_rs['Subject1_ID'];
-            $sub2_ID = $find_rs['Subject2_ID'];
-            $sub3_ID = $find_rs['Subject3_ID'];
-        
-            $all_subjects = array($sub1_ID, $sub2_ID, $sub3_ID);
+<p>
+    <?php
+        $sub1_ID = $find_rs['Subject1_ID'];
+        $sub2_ID = $find_rs['Subject2_ID'];
+        $sub3_ID = $find_rs['Subject3_ID'];
     
-            // loop through subjects ID's and look up the subject name
-            foreach($all_subjects as $subject) {
-                // Get subject name
-                $sub_sql ="SELECT * FROM `subject` WHERE `Subject_ID` = $subject";
-                $sub_query = mysqli_query($dbconnect, $sub_sql);
-                $sub_rs = mysqli_fetch_assoc($sub_query);
+        $all_subjects = array($sub1_ID, $sub2_ID, $sub3_ID);
+
+        // loop through subjects ID's and look up the subject name
+        foreach($all_subjects as $subject) {
+            // Get subject name
+            $sub_sql ="SELECT * FROM `subject` WHERE `Subject_ID` = $subject";
+            $sub_query = mysqli_query($dbconnect, $sub_sql);
+            $sub_rs = mysqli_fetch_assoc($sub_query);
+            
+            if($subject != 0)
+            {
                 
-                if($subject != 0)
-                {
-                    
-                ?>
-            <!-- show subjects -->
-            <span class="tag">
-                <a href="index.php?page=subject&subjectID=<?php echo
-                $sub_rs['Subject_ID']; ?>">
-                    <?php echo $sub_rs['Subject']; ?>
-                </a>
-            </span> &nbsp;
-        
-        <?php    
+            ?>
+        <!-- show subjects -->
+
+        <!-- OKAY WOOJIN SO I THINK I AM 
+        MISSING A WHOLE SUBJECT.PHP FILE 
+        PLEASE ASK MISS ABOUT THIS
+        VERY CRUICAL -->
+
+
+        <span class="tag">
+            <a href="index.php?page=subject&subjectID=<?php echo
+            $sub_rs['Subject_ID']; ?>">
+                <?php echo $sub_rs['Subject']; ?>
+            </a>
+        </span> &nbsp;
                 
-            } // end subject exists if
-    
-            unset($subject);
-                
-            } // end subject loop
-    
-        ?>
-    
-    </p>
+    <?php    
+            
+        } // end subject exists if
+
+        unset($subject);
+            
+        } // end subject loop
+
+    ?>
+
+</p>
