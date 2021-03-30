ab3f6a7c25aa1a48754a6450d5fd3c389242e038
Woojin Jeon <69278138+jeonw70450@users.noreply.github.com>
2021-03-30 19:02:33 +1300

    Update add_entry.php

--- admin/add_entry.php
+++ admin/add_entry.php
@@ -3,18 +3,10 @@
 if (isset($_SESSION['admin'])) {
 
     $author_ID = $_SESSION['Add_Quote'];
-<<<<<<< HEAD
 
     $all_tags_sql = "SELECT * FROM `subject` ORDER BY `Subject` ASC";
     $all_subjects = autocomplete_list($dbconnect, $all_tags_sql, 'Subject');
 
-=======
-    echo "Author:ID: ".$author_ID;
-    
-    $all_tags_sql = "SELECT * FROM `subject` ORDER BY `Subject_ID` ASC";
-    $all_subjects = autocomplete_list($dbconnect, $all_tags_sql, 'Subject_ID');
-    
->>>>>>> parent of f9f70a1 (cont.)
     $quote = "Please type your quote here";
     $notes = "";
     $tag_1 = "";
@@ -29,7 +21,6 @@ if (isset($_SESSION['admin'])) {
     $quote_field = "form-ok";
     $tag_1_field = "tag-ok";
 
-<<<<<<< HEAD
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $quote = mysqli_real_escape_string($dbconnect, $_POST['quote']);
@@ -50,7 +41,7 @@ if (isset($_SESSION['admin'])) {
             $tag_1_error = "error-text";
             $tag_1_field = "form-error";
         }
-        
+
         if ($has_errors != "yes") {
 
         $subjectID_1 = get_ID($dbconnect, 'subject', 'Subject_ID', 'Subject', $tag_1);
@@ -62,24 +53,6 @@ if (isset($_SESSION['admin'])) {
             '$author_ID', '$quote', '$notes', '$subjectID_1', '$subjectID_2', '$subjectID_3');";
         $addentry_query = mysqli_query($dbconnect, $addentry_sql);
 
-=======
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    $quote = mysqli_real_escape_string($dbconnect, $_POST['quote']);
-    $notes = mysqli_real_escape_string($dbconnect, $_POST['notes']);
-    
-    
-    
-    if ($quote == "Please type your quote here") {
-        $has_errors= "yes";
-        $quote_error = "error-text";
-        $quote_field = "form-error";
-        }
-}    
-    
-    
-}
->>>>>>> parent of f9f70a1 (cont.)
 
         $get_quote_sql = "SELECT * FROM `quotes` WHERE 'Quote' LIKE '$quote'";
         $get_quote_query = mysqli_query($dbconnect, $get_quote_sql);
@@ -108,7 +81,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="<?php echo $quote_error; ?>">
         This field cannot be blank
     </div>
-<<<<<<< HEAD
 
     <textarea class="add-field <?php echo $quote_field ?>" name="quote" rows="6"><?php echo $quote; ?></textarea>
     <br /><br />
@@ -118,25 +90,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <br /><br />
 
     <div class="<?php echo $tag_1_error; ?>">
-=======
-    
-    <textarea class="add-field <?php echo $quote_field?>" name="quote"
-    rows="6"><?php echo $quote; ?></textarea>
-    <br/><br />
-    
-        <input class="add-field <?php echo $notes; ?>" type="text" name="notes" value="<?php echo $notes; ?>" placeholder="Notes (optional) ..."/>
-    
-    <br/><br />
-    
-        <div class="<?php $tag_1_error ?>">
->>>>>>> parent of f9f70a1 (cont.)
         Please enter at least one subject tag
     </div>
 
     <div class="autocomplete">
         <input class="<?php echo $tag_1_field; ?>" id="subject1" type="text" name="Subject_1" placeholder="Subject 1 (Start Typing)...">
     </div>
-<<<<<<< HEAD
 
     <br /><br />
 
@@ -150,9 +109,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <input id="subject3" type="text" name="Subject_3" placeholder="Subject 3 (Start Typing)...">
     </div>
 
-=======
-    
->>>>>>> parent of f9f70a1 (cont.)
     <p>
         <input type="submit" value="Submit" />
     </p>
@@ -163,15 +119,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 <script>
-<<<<<<< HEAD
     <?php include("autocomplete.php"); ?>;
-=======
-<?php include("autocomplete.php"); ?>
-
-    
-var all_tags = <?php print("$all_subjects"); ?>;
-autocomplete(document.getElementById("subject1"), all_tags);
->>>>>>> parent of f9f70a1 (cont.)
 
 
     var all_tags = <?php print("$all_subjects"); ?>;
